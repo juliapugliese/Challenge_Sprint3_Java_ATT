efebf0ed34c41e8b6752a5108d510b0ff0c8bebf
Julia Pugliese <julia@jujubooks.com.br>
2024-03-28 21:40:26 -0300

    Preparação do ambiente

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,11 @@
 package org.example;
 
-import org.example.entities.ServicoModel.Plano;
-import org.example.entities.ServicoModel.Produto;
-import org.example.entities.UsuarioModel.Administrador;
-import org.example.entities.UsuarioModel.Cliente;
-import org.example.repositories.PlanosRepository;
-import org.example.repositories.ProdutosRepository;
-import org.example.repositories.UsuariosRepository;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import java.io.IOException;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Main class.
@@ -47,10 +38,10 @@ public class Main {
 
         //TESTE
 //        new UsuariosRepository().initialize();
-        var adm2 = new Administrador("553427", "fiap2", "Julia Ribeiro", "rm553427@fiap.com.br");
-        var cli = new Cliente("juliana", "juliana789", "juliana G. P. Ribeiro", 14451445751L, "11456699887", "fiap", 14563201478965L, "Aluno", "Analise e Desenvolvimento de Sistemas", "Medio", "Brasil", "rm553427@fiap.com.br");
-        var administradores = new UsuariosRepository();
-        var clientes = new UsuariosRepository();
+//        var adm2 = new Administrador("553427", "fiap2", "Julia Ribeiro", "rm553427@fiap.com.br");
+//        var cli = new Cliente("juliana", "juliana789", "juliana G. P. Ribeiro", 14451445751L, "11456699887", "fiap", 14563201478965L, "Aluno", "Analise e Desenvolvimento de Sistemas", "Medio", "Brasil", "rm553427@fiap.com.br");
+//        var administradores = new UsuariosRepository();
+//        var clientes = new UsuariosRepository();
 //        clientes.create(cli);
 //        administradores.create(adm2);
 //        clientes.readAll();
@@ -65,8 +56,8 @@ public class Main {
 //        new UsuariosRepository().shutdown();
 //
 //        new PlanosRepository().initialize();
-        var planscss = new Plano("Premium", "Explore recursos autoguiados, como aprendizado online, demonstrações e conselhos da comunidade. Incluído em todas as licenças", "Trailhead, Portal de Ajuda, Comunidade de Trailblazers, Success Center, Suporte Técnico");
-        var repopl = new PlanosRepository();
+//        var planscss = new Plano("Premium", "Explore recursos autoguiados, como aprendizado online, demonstrações e conselhos da comunidade. Incluído em todas as licenças", "Trailhead, Portal de Ajuda, Comunidade de Trailblazers, Success Center, Suporte Técnico");
+//        var repopl = new PlanosRepository();
 //        repopl.create(planscss);
 //        repopl.readAll();
 //        repopl.delete(2);
@@ -77,18 +68,18 @@ public class Main {
 //        new PlanosRepository().shutdown();
 //
 //        new ProdutosRepository().initialize();
-        var produto = new Produto("Sales Cloudy",  "Venda mais rápido e com mais inteligência com qualquer uma das nossas edições de CRM totalmente personalizáveis.", new ArrayList<>(List.of(
-                new Plano("Starter", "Ferramentas de vendas e atendimento ao cliente em um app", 25),
-                new Plano("Sales Professional", "Solução de vendas completa para equipes de qualquer tamanho",  80),
-                new Plano("Enterprise", "CRM de vendas altamente personalizável para o seu negócio",  165),
-                new Plano("Unlimited", "A plataforma definitiva para seu crescimento",  330)
-        )));
-
-        var produto1 = new Produto("MuleSoft Automation",  "Combine o poder do MuleSoft RPA, MuleSoft Composer e Anypoint Platform para ajudar as equipes a automatizar.", new ArrayList<>(List.of(
-                new Plano("MuleSoft Automation", "Capacite suas equipes para fazer mais, com menos.",  4750)
-        )));
+//        var produto = new Produto("Sales Cloudy",  "Venda mais rápido e com mais inteligência com qualquer uma das nossas edições de CRM totalmente personalizáveis.", new ArrayList<>(List.of(
+//                new Plano("Starter", "Ferramentas de vendas e atendimento ao cliente em um app", 25),
+//                new Plano("Sales Professional", "Solução de vendas completa para equipes de qualquer tamanho",  80),
+//                new Plano("Enterprise", "CRM de vendas altamente personalizável para o seu negócio",  165),
+//                new Plano("Unlimited", "A plataforma definitiva para seu crescimento",  330)
+//        )));
 //
-        var prodrepo = new ProdutosRepository();
+//        var produto1 = new Produto("MuleSoft Automation",  "Combine o poder do MuleSoft RPA, MuleSoft Composer e Anypoint Platform para ajudar as equipes a automatizar.", new ArrayList<>(List.of(
+//                new Plano("MuleSoft Automation", "Capacite suas equipes para fazer mais, com menos.",  4750)
+//        )));
+////
+//        var prodrepo = new ProdutosRepository();
 //        prodrepo.create(produto);
 //        prodrepo.delete(2);
 //        prodrepo.readAll();

0cc94171389b63ea779bc438c57dc14c92aac6e5
Julia Pugliese <julia@jujubooks.com.br>
2024-05-17 23:38:15 -0300

    Adição de novas dependências

--- src/main/java/org/example/infrastructure/CorsFilter.java
+++ src/main/java/org/example/infrastructure/CorsFilter.java
@@ -15,7 +15,7 @@ public class CorsFilter implements ContainerResponseFilter {
                        ContainerResponseContext responseContext) throws IOException {
         responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
         responseContext.getHeaders().add("Access-Control-Allow-Credentials", "true");
-        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
-        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "origin, content-type, Content-Type, accept, authorization");
+        responseContext.getHeaders().add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD, CONNECT");
     }
 }
